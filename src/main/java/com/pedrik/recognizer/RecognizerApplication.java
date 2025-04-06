f576d84c2add6eaf624a9a7e644fff45b6fa9eac
EForamiglio <erik.foramiglio.vieira@gmail.com>
2025-04-06 17:45:33 -0300

    init project

---
+++ src/main/java/com/pedrik/recognizer/RecognizerApplication.java
@@ -0,0 +1,13 @@
+package com.pedrik.recognizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecognizerApplication.class, args);
+	}
+
+}
